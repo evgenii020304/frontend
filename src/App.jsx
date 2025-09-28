f1724f7b4d84f341de37cbe74ab8f4e81e931c8b
Evgeniy Naumov <ecgeniinaumov04@gmail.com>
2025-09-29 02:14:18 +0300

    frontend 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 function App() {
     const [inputValue, setInputValue] = useState('');
+    const [outputValue, setOutputValue] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -29,13 +30,29 @@ function App() {
             }
         } catch (error) {
             console.error('Ошибка:', error);
+            alert('Не удалось подключиться к серверу.');
+        }
+    };
+
+    const fetchLastData = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/data');
+
+            if (response.ok) {
+                const data = await response.json();
+                setOutputValue(data.content);
+            } else {
+                setOutputValue(`Ошибка: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
             alert('Не удалось подключиться к серверу. Убедитесь, что бэкенд запущен.');
         }
     };
 
     return (
         <div className="container">
-            <h1 className='container__title'>Задание для frontend 1.0</h1>
+            <h1 className="container__title">Задание для frontend 1.0</h1>
             <form onSubmit={handleSubmit} className="container__form">
                 <div className="container__input-group">
                     <input
@@ -50,6 +67,24 @@ function App() {
                     </button>
                 </div>
             </form>
+
+            <h1 className='container__title'>Задание для frontend 2.0</h1>
+            <div className="container__output-group">
+                <button
+                    type="button"
+                    className="container__button"
+                    onClick={fetchLastData}
+                >
+                    Получить данные
+                </button>
+                <input
+                    type="text"
+                    className="container__input"
+                    placeholder="Здесь появится последняя запись из файла..."
+                    value={outputValue}
+                    readOnly
+                />
+            </div>
         </div>
     );
 }
